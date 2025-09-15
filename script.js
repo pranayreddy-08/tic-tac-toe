5bde4a8b933208c233c32f1acb83d3f239e50b08
Pranay Reddy <pranayreddyedmala@gmail.com>
2025-09-15 12:54:32 +0530

    Initial commit: Tic Tac Toe game

---
+++ script.js
@@ -0,0 +1,67 @@
+let boxes=document.querySelectorAll('.cell');
+let resetbutton=document.querySelector('#resetButton');
+let winner=document.querySelector('.winner');
+let turn=true; //true for X and false for O
+const winPatterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+boxes.forEach(box=>{
+    box.addEventListener('click',()=>{
+        if(turn){
+            box.innerText='X';
+            turn=false;
+        }else{
+            box.innerText='O';
+            turn=true;
+        }
+        box.disabled=true;
+        checkwinner();
+    });
+});
+const hidebtns=()=>{
+    boxes.forEach(box=>{
+        box.disabled=true;
+    });
+};
+const checkwinner = () => {
+    setTimeout(() => {
+        for (let pattern of winPatterns) {
+            const [a, b, c] = pattern;
+            if (
+                boxes[a].innerText &&
+                boxes[a].innerText === boxes[b].innerText &&
+                boxes[a].innerText === boxes[c].innerText
+            ) {
+                winner.innerText = `Player ${boxes[a].innerText} wins!`;
+                winner.classList.remove('hide');
+                hidebtns();
+                return;
+            }
+        }
+        if ([...boxes].every(box => box.innerText)) {
+            winner.innerText = "It's a draw!";
+            winner.classList.remove('hide');
+        }
+    }, 50);
+};
+const resetGame=()=>{
+    boxes.forEach(box=>{
+        box.innerText='';
+        box.disabled=false;
+    });
+    turn=true;
+    winner.classList.add('hide');
+};
+resetbutton.addEventListener('click',resetGame);
+
+
+
+
+
